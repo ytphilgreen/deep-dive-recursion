9b92043b61547ef8065bc6a3fee6347c764369d0
yolanda philgreen <yolanda.philgreen@gmail.com>
2017-06-19 15:41:27 -0600

    palindrome project

---
+++ src/edu/cnm/deepdive/recursion/Fibonacci.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.recursion;
+
+import java.math.BigInteger;
+
+public class Fibonacci {
+  
+  public static void main(String[]args){
+    
+  }
+public static BigInteger fib (int n){
+  if (n <= 0){
+    return BigInteger.ZERO;
+  }
+  if (n == 1) {
+    return BigInteger.ONE;
+  }
+  return fib(n -1).add(fib(n-2));
+}
+
+}
